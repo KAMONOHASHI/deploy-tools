7791656f273b7c76e40f8e56218fe13cda3359d8
yonezawa.tatsuki <yonezawa.tatsuki.eh8@jp.nssol.nipponsteel.com>
2021-12-20 14:43:47 +0900

    クレデンシャルを設定する関数の処理を変更

--- kamonohashi/deploy-kqi-app.sh
+++ kamonohashi/deploy-kqi-app.sh
@@ -6,15 +6,22 @@ show_help() {
     echo "available args: prepare, deploy, clean, update, credentials, upgrade, help"
 }
 
-
-set_credentials(){   
+set_credentials(){
     kubectl apply -f kqi-namespace.yml
-    if [ -z "$PASSWORD" ] || [ -z "$DB_PASSWORD" ] || [ -z "$STORAGE_PASSWORD" ]; then
-      echo -en "\e[33mAdmin Passwordを入力: \e[m"; read -s PASSWORD
-      echo -en "\n\e[33mDB Passwordを入力: \e[m"; read -s DB_PASSWORD
-      echo -en "\n\e[33mStorage Secret Keyを入力: \e[m"; read -s STORAGE_PASSWORD
-      echo "" # read -sは改行しないので改行 
-    fi  
+    # パスワードが未指定のものは、既にSecretに設定されているパスワードを設定する
+    if [ -z "$PASSWORD" ]; then
+      PASSWORD=`kubectl get secret --namespace kqi-system platypus-web-api-env-secret -o jsonpath="{.data.DeployOptions__Password}" | base64 --decode` 
+    fi
+    if [ -z "$DB_PASSWORD" ]; then
+      DB_PASSWORD=`kubectl get secret --namespace kqi-system postgres-credential -o jsonpath="{.data.POSTGRES_PASSWORD}" | base64 --decode`
+    fi
+    if [ -z "$STORAGE_PASSWORD" ]; then
+      STORAGE_PASSWORD=`kubectl get secret --namespace kqi-system minio-credential -o jsonpath="{.data.MINIO_ROOT_PASSWORD}" | base64 --decode`
+      # MinIOが以前のバージョンだと環境変数名は"MINIO_SECRET_KEY"なので、そこからパスワードを取得する
+      if [ -z "$STORAGE_PASSWORD" ]; then
+        STORAGE_PASSWORD=`kubectl get secret --namespace kqi-system minio-credential -o jsonpath="{.data.MINIO_SECRET_KEY}" | base64 --decode`
+      fi
+    fi
     SET_ARGS="password=$PASSWORD,db_password=$DB_PASSWORD,storage_secretkey=$STORAGE_PASSWORD"
     helm upgrade kamonohashi-credentials charts/kamonohashi-credentials -i --set $SET_ARGS --namespace kqi-system
 }
